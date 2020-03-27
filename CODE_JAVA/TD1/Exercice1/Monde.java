cc3c2100f95509f4e2628738c02ed30a2771d2bc
Avi Assayag <avi_assayag22@hotmail.fr>
2020-03-27 16:15:05 +0100

    Exo1 Java

---
+++ CODE_JAVA/TD1/Exercice1/Monde.java
@@ -0,0 +1,29 @@
+package CODE_JAVA.TD1;
+
+/*
+ Exercice 1 : Premier programme java : Lecture et affichage 
+ 
+ Question 1 : 
+ 
+public class Monde {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+Question 2
+*/
+
+public class Monde {
+    public String nom;
+    Monde(String nom){
+            this.nom = nom;
+    } 
+    public static void main(String[] args) {
+        Monde A = new Monde (args[0]);
+        System.out.println("Hello " + A.nom);
+    }
+}
+
+
+
+

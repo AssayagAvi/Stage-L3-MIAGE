52e2e6e8b5618a5781ff87767993cf45563b4958
Avi Assayag <avi_assayag22@hotmail.fr>
2020-03-27 18:17:54 +0100

    TD1 Exercice1

---
+++ JAVA/TD1/Exercice1/Monde.java
@@ -0,0 +1,43 @@
+package JAVA.TD1.Exercice1;
+
+/*
+ Exercice 1 : Premier programme java : Lecture et affichage 
+ 
+ Question 1 : 
+ 
+public class Monde {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+Question 2
+
+public class Monde {
+    public String nom;
+    Monde(String nom){
+            this.nom = nom;
+    } 
+    public static void main(String[] args) {
+        Monde A = new Monde (args[0]);
+        System.out.println("Hello " + A.nom);
+    }
+}
+ Question 3 
+ */
+
+public class Monde {
+    public String nom ;
+    Monde(String nom){
+        this.nom = nom;
+    }
+
+    public static void main(String[] args) {
+       for (int i = 0; i < args.length; i++) {
+        Monde B = new Monde(args[i]);
+        System.out.println(i + " Hello " + B.nom);
+       }
+    }
+}
+
+
+
